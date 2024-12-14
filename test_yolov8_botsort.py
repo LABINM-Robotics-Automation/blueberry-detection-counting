a32356b3072e2d497b324f621900afc46e8f9b83
pQbas <percybrayan18@gmail.com>
2024-12-14 13:09:33 -0500

    minimal changes at importing operations

--- test_yolov8_botsort.py
+++ test_yolov8_botsort.py
@@ -1,14 +1,14 @@
-import ypyb.methods.yolov8_botsort.ops as ops
+import ypyb.methods.yolov8_botsort.ops as o
 import ypyb.methods.yolov8_botsort.classes as c
 import cv2
 import os
 
-image = cv2.imread(f'{os.getcwd()}/gallery/cat.jpeg')
 conf  = 0.5
+image = cv2.imread(f'{os.getcwd()}/gallery/cat.jpeg')
 model = c.Yolo8('yolov8n.pt', device='cuda')
 
+detection = o.detect_on_image(image, conf, model)
 
-detection = ops.detect_on_image(image, conf, model)
 
 cv2.imshow('Image', detection)
 
