2e78f3787cf438371762b436dc54957c454f915d
pQbas <percybrayan18@gmail.com>
2024-12-14 13:23:18 -0500

    add class Yolov9,10,11 to their respective folders

---
+++ ypyb/methods/yolov10_botsort/classes.py
@@ -0,0 +1,36 @@
+from ultralytics import YOLO
+from ultralytics.data.augment import LetterBox
+import cv2
+import torch
+
+class Yolo10:
+    def __init__(self, weights, device):
+        self.weights = weights
+        self.device = device
+        self.model = YOLO(self.weights, verbose=False)
+        self.letterbox = LetterBox()
+
+    def detect(self, img, conf_thres=0.5):
+        '''
+
+        Args
+            img        : is input where we perform object detection
+            conf_thres : float between 0 and 1 
+        '''
+        results = None
+        return results 
+
+
+    def plot_prediction(self, img, results):
+        '''
+
+        Args
+            img     : is input where we perform object detection
+            results : are the output of the model (what returns detect method) 
+        '''
+        img = None
+
+        return img
+
+
+
