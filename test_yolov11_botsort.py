79c7ef652de27de545641adb44880f1db593d858
pQbas <percybrayan18@gmail.com>
2024-12-15 17:03:07 -0500

    add test for every current yolo version

--- test_yolov11_botsort.py
+++ test_yolov11_botsort.py
@@ -1,11 +1,11 @@
-import ypyb.methods.yolov8_botsort.ops as o
-import ypyb.methods.yolov8_botsort.classes as c 
+import ypyb.methods.yolov11_botsort.ops as o
+import ypyb.methods.yolov11_botsort.classes as c 
 import cv2
 import os
 
 conf  = 0.5
 image = cv2.imread(f'{os.getcwd()}/gallery/cat.jpeg')
-model = c.Yolo8('yolov8n.pt', device='cuda')
+model = c.Yolo11('yolov11n.pt', device='cuda')
 
 detection = o.detect_on_image(image, conf, model)
 
@@ -15,4 +15,3 @@ while True:
     key = cv2.waitKey(0)
     if key == ord('q'):
         cv2.destroyAllWindows()
-
--- test_yolov9_botsort.py
+++ test_yolov9_botsort.py
@@ -1,11 +1,11 @@
-import ypyb.methods.yolov8_botsort.ops as o
-import ypyb.methods.yolov8_botsort.classes as c 
+import ypyb.methods.yolov9_botsort.ops as o
+import ypyb.methods.yolov9_botsort.classes as c 
 import cv2
 import os
 
 conf  = 0.5
 image = cv2.imread(f'{os.getcwd()}/gallery/cat.jpeg')
-model = c.Yolo8('yolov8n.pt', device='cuda')
+model = c.Yolo9('yolov9n.pt', device='cuda')
 
 detection = o.detect_on_image(image, conf, model)
 
